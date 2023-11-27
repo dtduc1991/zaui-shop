ec9b33fce63c45212fa718f77a4c7f7ef7be68ef
Duc Dang <dtduc1991@gmail.com>
2023-11-27 21:34:18 +0700

    Added products

--- src/mock/benefits.js
+++ src/mock/benefits.js
@@ -7,22 +7,22 @@ export const benefitContent = [
     imgSrc: "/images/benefit-icons/trai_tuoi.png",
     title: "Trái tươi",
   },
-  {
-    imgSrc: "/images/benefit-icons/005-delivery-truck-2.webp",
-    title: "Giao hàng nhanh",
-  },
-  {
-    imgSrc: "/images/benefit-icons/003-cash-on-delivery.webp",
-    title: "Nhận tiền mặt",
-  },
-  {
-    imgSrc: "/images/benefit-icons/004-headphones.webp",
-    title: "Hỗ trợ 24/7",
-  },
-  {
-    imgSrc: "/images/benefit-icons/006-best-seller.webp",
-    title: "Bảo đảm chất lượng",
-  },
+  // {
+  //   imgSrc: "/images/benefit-icons/005-delivery-truck-2.webp",
+  //   title: "Giao hàng nhanh",
+  // },
+  // {
+  //   imgSrc: "/images/benefit-icons/003-cash-on-delivery.webp",
+  //   title: "Nhận tiền mặt",
+  // },
+  // {
+  //   imgSrc: "/images/benefit-icons/004-headphones.webp",
+  //   title: "Hỗ trợ 24/7",
+  // },
+  // {
+  //   imgSrc: "/images/benefit-icons/006-best-seller.webp",
+  //   title: "Bảo đảm chất lượng",
+  // },
   // {
   //   imgSrc: "/images/benefit-icons/007-return.png",
   //   title: "return",
